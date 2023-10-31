d7c5d5a266de1e704dd71d8d61e5053e78382aec
Reto Stamm <reto@retostamm.com>
2023-10-30 21:56:48 -0700

    Fixed naming

--- src/tt_um_retospect_neurochip.v
+++ src/tt_um_retospect_neurochip.v
@@ -1,8 +1,7 @@
 
 
-module tt_um_retospect_neurochip #(
-    parameter MAX_COUNT = 24'd10_000_000
-) (
+module tt_um_retospect_neurochip
+(
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
@@ -13,9 +12,9 @@ module tt_um_retospect_neurochip #(
     input  wire       rst_n     // reset_n - low to reset
 );
 
-always @(posedge clk) begin
-	uio_out <= uio_in;
-end
+  always @(posedge clk) begin
+    uio_out <= uio_in;
+  end
 
 endmodule  // tt_retospect
 
