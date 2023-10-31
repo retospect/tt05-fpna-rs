db7c317827e8f313a29984c3b6e23b132714af26
Reto Stamm <reto@retostamm.com>
2023-10-31 16:17:21 -0700

    Minimal

--- src/tt_um_retospect_neurochip.v
+++ src/tt_um_retospect_neurochip.v
@@ -13,25 +13,11 @@ module tt_um_retospect_neurochip #( parameter MAX_COUNT = 24'd10_000_000 ) (
 
     wire reset = ! rst_n;
     wire [6:0] led_out;
-    assign uo_out[6:0] = led_out;
-    assign uo_out[7] = 1'b0;
 
     // use bidirectionals as outputs
-    assign uio_oe = 8'b11111111;
-
-    // put bottom 8 bits of second counter out on the bidirectional gpio
-    assign uio_out = second_counter[7:0];
-
-    // external clock is 10MHz, so need 24 bit counter
-    reg [23:0] second_counter;
-    reg [3:0] digit;
-
-    // if external inputs are set then use that as compare count
-    // otherwise use the hard coded MAX_COUNT
-    wire [23:0] compare = ui_in == 0 ? MAX_COUNT: {6'b0, ui_in[7:0], 10'b0};
+    assign uio_oe = 8'b11110000;
 
 
-    // instantiate segment display
-    seg7 seg7(.counter(digit), .segments(led_out));
+
 
 endmodule
