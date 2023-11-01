98405cf92aee116e62e6f15423cd12b65f07cd85
Reto Stamm <reto@retostamm.com>
2023-10-31 19:22:19 -0700

    Stupid shift

--- src/test.py
+++ src/test.py
@@ -2,11 +2,15 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
-
-segments = [ 63, 6, 91, 79, 102, 109, 125, 7, 127, 111 ]
+bitstream_length = 10
 
 @cocotb.test()
-async def test_7seg(dut):
+async def test_shiftreg(dut):
+    config_en = dut.uio_in[3]
+    bs_in     = dut.uio_in[2]
+    bs_out    = dut.uio_out[1]
+    bs_in.value = 0 
+
     dut._log.info("start")
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
@@ -15,34 +19,22 @@ async def test_7seg(dut):
     dut._log.info("reset")
     dut.rst_n.value = 0
     # set the compare value
-    dut.ui_in.value = 1
+    
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    # the compare value is shifted 10 bits inside the design to allow slower counting
-    max_count = dut.ui_in.value << 10
-    dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
-    # check all segments and roll over
-    for i in range(15):
-        dut._log.info("check segment {}".format(i))
-        await ClockCycles(dut.clk, max_count)
-        #assert int(dut.segments.value) == segments[i % 10]
+    config_en.value = 1 
+    # initialize the bitstream buffer.
+    await ClockCycles(dut.clk, bitstream_length+10)
 
-        # all bidirectionals are set to output
-        #assert dut.uio_oe == 0xFF
-    assert True
-    # reset
-    dut.rst_n.value = 0
-    # set a different compare value
-    dut.ui_in.value = 3
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+    bs_in.value = 1
+    await ClockCycles(dut.clk, 1)
+    bs_in.value = 0
+    for i in range(bitstream_length-1):
+        await ClockCycles(dut.clk, 1)
+        print(i, bs_out.value)
+        assert bs_out.value == 0
+    await ClockCycles(dut.clk, 1)
+    assert bs_out.value == 1
 
-    max_count = dut.ui_in.value << 10
-    dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
-    # check all segments and roll over
-    for i in range(15):
-        dut._log.info("check segment {}".format(i))
-        await ClockCycles(dut.clk, max_count)
-        #assert int(dut.segments.value) == segments[i % 10]
 
--- src/tt_um_retospect_neurochip.v
+++ src/tt_um_retospect_neurochip.v
@@ -15,15 +15,30 @@ module tt_um_retospect_neurochip #( parameter MAX_COUNT = 24'd10_000_000 ) (
 
     // use bidirectionals as outputs
     assign uio_oe = 8'b11000010;
+
     wire [0:9] inbus;
     assign inbus = {ui_in[7:0], uio_in[7:6]};
+
     wire [0:9] outbus;
     assign {uo_out[7:0], uio_out[5:4]} = outbus;
+
     wire config_en = uio_in[3];
     wire bs_in = uio_in[2];
-    wire bs_out = uio_out[1];
+    wire bs_out;
+    assign uio_out[1] = bs_out;
     wire reset_nn = uio_in[0];
-    assign bs_out = 0;
+
+    reg [0:9] bs = 0;
+    // on each rising edge, if config_en is high, load the value from bs_in
+    // into the bs register and set bs_out to the output of the bs register
+    // and shfit the bs register left by one bit
+    always @(posedge clk) begin
+	if (config_en) begin
+	    bs[0:9] <= {bs_in,bs[0:8]};
+	end
+    end
+
+    assign bs_out = bs[9];
     assign outbus = 10'b0000000000;
 
 endmodule
